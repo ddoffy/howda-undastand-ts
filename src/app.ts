a7cb5f68a9bb65dac4a3e7cd9d085777dcd68daa
D. Doffy <cuongnsm@gmail.com>
2024-07-14 22:50:35 -0600

    Add a project for POC
    Signed-off-by: D. Doffy <cuongnsm@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,51 @@
-console.log("Hello World");
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
-function println(msg: string | number): void {
-  console.log(msg);
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private configure(): void {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  private submitHandler(event: Event): void {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private attach(): void {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
 }
 
-function add(a: number, b: number): number {
-  return a + b;
-}
-
-function sumary(a: number, b: number, cb: (result: number) => void) {
-  cb(add(a, b));
-}
-
-sumary(1, 2, println);
+const prjInput = new ProjectInput();
